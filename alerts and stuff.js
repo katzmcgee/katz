4048a90170adcfbcbe16628ecc30e3761c1a0b09
katzmcgee <lemuelyu111@gmail.com>
2020-11-01 22:11:17 -0500

    Update alerts and stuff.js

--- alerts and stuff.js
+++ alerts and stuff.js
@@ -1,1 +1,1 @@
-alert("WARNING!!! \nBe ready to boop Katz on the snoot.\nPlease exit this page IMMEDIATELY if you listen to K-pop, play with spidget finners or are otherwise mentally disabled.");
+alert("WARNING!!! \nBe ready to boop Katz on the snoot.\nPlease exit this page IMMEDIATELY if you listen to K-pop, play with spidget finners or are otherwise dumb.");
